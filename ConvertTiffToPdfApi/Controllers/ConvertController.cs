3d9aa568e5ba694e7a7e6fa735d0c93d557e191f
Eric Segelberg <eric.segelberg@ersu.se>
2023-02-20 23:14:29 +0100

    Still same issue, but cleaned up memory to make it easier to compare memory usage between IsBlackWhite=true/false

--- ConvertTiffToPdfApi/Controllers/ConvertController.cs
+++ ConvertTiffToPdfApi/Controllers/ConvertController.cs
@@ -10,12 +10,19 @@ namespace ConvertTiffToPdfApi.Controllers
     [Route("api/[controller]")]
     public class ConvertController : Controller
     {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ConvertController(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
         [HttpPost]
         [Route("")]
         [RequestSizeLimit(1073741824)]
         [RequestFormLimits(MultipartBodyLengthLimit = 1073741824)]
         [DisableRequestSizeLimit]
-        public async Task<IActionResult> Index(ConvertRequest request)
+        public IActionResult Index(ConvertRequest request)
         {
             var response = ConvertToPdf(request);
             return Ok(response);
@@ -23,19 +30,17 @@ namespace ConvertTiffToPdfApi.Controllers
 
         private string ConvertToPdf(ConvertRequest request)
         {
-
-            List<string> resultfileList = new List<string>();
+            var resultfileList = new List<string>();
             var conformanceLevel = request.PdfAConformanceLevel;
-
 
             foreach (var file in request.FileRequestModels)
             {
+                using var pdfHandler = _serviceProvider.GetRequiredService<PDFHandler>();
                 switch (file.Type.ToLower())
                 {
-
                     case "tiff":
                     case "tif":
-                        resultfileList.Add(ConvertTiffToPdf(file));
+                        resultfileList.Add(pdfHandler.ConvertTiffToPdf(file.FileBase64));
                         break;
 
                     default:
@@ -43,112 +48,10 @@ namespace ConvertTiffToPdfApi.Controllers
                 }
             }
 
-            var mergedPdf = MergePdf(resultfileList, request.ResultFileName, conformanceLevel);
+            var handler = _serviceProvider.GetRequiredService<PDFHandler>();
+            var mergedPdf = handler.MergePdf(resultfileList, request.ResultFileName, conformanceLevel);
             return mergedPdf;
         }
-
-        private string ConvertTiffToPdf(FileRequestModel file)
-        {
-            try
-            {
-                /*using*/
-                var pdf = new Aspose.Pdf.Document();
-                /*using*/
-                var pdfStream = new MemoryStream();
-                /*using*/
-                var ms = new MemoryStream(Convert.FromBase64String(file.FileBase64));
-                /*using*/
-                var myImage = new Bitmap(ms);
-
-                var dimension = new FrameDimension(myImage.FrameDimensionsList[0]);
-                var frameCount = myImage.GetFrameCount(dimension);
-
-                //var currentImageList = new List<MemoryStream>();
-                for (int frameIdx = 0; frameIdx <= frameCount - 1; frameIdx++)
-                {
-
-                    var page = pdf.Pages.Add();
-
-                    myImage.SelectActiveFrame(dimension, frameIdx);
-                    var currentImage = new MemoryStream();
-                    //currentImageList.Add(currentImage);
-                    myImage.Save(currentImage, ImageFormat.Tiff);
-                    var image = new Aspose.Pdf.Image { ImageStream = currentImage };
-
-
-                    //image = DownScaleImageToA4(myImage.Height, myImage.Width, image);
-                    //SetPageInfo(page);
-
-                    //Denna flagga gör så att det ibland smäller oväntat. Nackdelen med att ha den av är att filstorleken blir enormt mycket större än om man har den på.
-                    //image.IsBlackWhite = true;
-
-
-
-                    page.Paragraphs.Add(image);
-                }
-
-                //OptimizePdf(pdf);
-
-                pdf.Save(pdfStream, Aspose.Pdf.SaveFormat.Pdf);
-                pdfStream.Position = 0;
-
-                //foreach(var stream in currentImageList)
-                //{
-                //    stream.Dispose();
-                //}
-
-                //pdf.FreeMemory();
-                return Convert.ToBase64String(pdfStream.ToArray());
-            }
-            catch (Exception e)
-            {
-
-                var test = e;
-                throw;
-            }
-        }
-
-        private string MergePdf(List<string> base64FileStrings, string resultFilename,
-            string conformanceLevel = null)
-        {
-            try
-            {
- 
-
-                var inputStreams = new MemoryStream[base64FileStrings.Count];
-                for (var i = 0; i < base64FileStrings.Count; i++)
-                {
-                    inputStreams[i] = new MemoryStream(Convert.FromBase64String(base64FileStrings[i]));
-                }
-
-                var pdfEditor = new PdfFileEditor();
-                var pdfStream = new MemoryStream();
-                pdfEditor.Concatenate(inputStreams, pdfStream);
-                var pdf = new Aspose.Pdf.Document(pdfStream);
-                pdf.Info.Title = resultFilename ?? "TS.AHS.PDF";
-
-                var returnStream = new MemoryStream();
-                pdf.Save(returnStream);
-
-                returnStream.Position = 0;
-                var base64 = Convert.ToBase64String(returnStream.ToArray());
-
-               
-  
-                return base64;
-            }
-
-            catch (Exception e)
-            {
-
-                var test = e;
-                throw;
-            }
-        }
-
-
-
-
     }
 
     public class ConvertRequest
--- ConvertTiffToPdfApi/Program.cs
+++ ConvertTiffToPdfApi/Program.cs
@@ -12,6 +12,7 @@ namespace ConvertTiffToPdfApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddTransient<PDFHandler>();
 
             var app = builder.Build();
 
